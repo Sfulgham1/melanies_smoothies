18f9426112ae3ae3bce6db3e03e4aa5731ee7cc6
Sfulgham1 <stefanie.fulgham@bcidaho.com>
2024-07-23 09:14:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['Fruit_Name']== fruit_chosen, 'Search_On'].iloc[0]
+        search_on=pd_df.loc[pd_df['Fruit_Name'] == fruit_chosen, 'Search_On'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
