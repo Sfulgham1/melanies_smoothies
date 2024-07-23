f605613ecfa80a4aa8ddef8bb95c20d60f7ce5a0
Sfulgham1 <stefanie.fulgham@bcidaho.com>
2024-07-23 09:09:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df=my_dataframe.to_panda()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
